348170ce6fb105df8248288674d505c8a776bfe7
ferraridiego <ferrari.diego92@gmail.com>
2022-03-23 18:27:25 -0300

    uptade

---
+++ script.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    origin:'top',
+    distance: '50px',
+    duration: 1000,
+    reset: true
+});
+
+ScrollReveal().reveal('.blog', {delay: 200 });
+ScrollReveal().reveal('.data-icon', {delay: 300 });
+ScrollReveal().reveal('.blogText', {delay: 400 });
